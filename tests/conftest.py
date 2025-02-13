ed9d8703b1aecb390390f6de15cb4d4f3625a90e
EthanLevine <elevine@bu.edu>
2025-02-13 15:46:55 -0500

    fixed test client

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from app import app
 
 @pytest.fixture
 def client():
+    """Flask test client fixture."""
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
